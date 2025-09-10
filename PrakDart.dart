bd9647e338c2a40ebf5ca19a0e69b769a80377de
dhanisaptrri <shadhanisa2@gmail.com>
2025-09-10 20:49:04 +0700

    Add initial Dart files with basic functionality
    - Created PrakDart.dart to print a message with age countdown from 21 to 1.
    - Created no4.dart to demonstrate the use of late initialization in Dart with a description string.

---
+++ PrakDart.dart
@@ -0,0 +1,5 @@
+void main() {
+  for (var i = 21; i > 0; i--) {
+    print('Nama saya adalah Dhanisa, sekarang berumur $i');
+  }
+}
---
+++ no4.dart
@@ -0,0 +1,6 @@
+void main() {
+  late String description;
+
+  description = "Flutter dengan Dart";
+  print(description);
+}
